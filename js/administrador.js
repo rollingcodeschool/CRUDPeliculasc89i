d3fec60ca28151c425a5da6cdcaaaa766d2ee4ee
Emilse Arias <emilsearias@gmail.com>
2024-03-18 20:25:02 -0300

    creamos la funcion creandoPelicula para administrar la logica de create

--- js/administrador.js
+++ js/administrador.js
@@ -1,11 +1,32 @@
 //variables globales
-const btnAgregarPelicula = document.getElementById('btnCrearPelicula');
-const modalPelicula = new bootstrap.Modal(document.getElementById('modalPelicula'))
+const btnAgregarPelicula = document.getElementById("btnCrearPelicula");
+const modalPelicula = new bootstrap.Modal(
+  document.getElementById("modalPelicula")
+);
+let crearPelicula = true;
+const formularioPelicula = document.getElementById("formPelicula");
 
 //funciones
-function mostrarModalPelicula(){
-    modalPelicula.show();
+function mostrarModalPelicula() {
+  crearPelicula = true;
+  modalPelicula.show();
+}
+
+function administrarFormularioPelicula(e) {
+  //aqui decidimos si estamos creando o editando una pelicula
+  e.preventDefault();
+  if (crearPelicula) {
+    //estoy creando la peli
+    creandoPelicula()
+  } else {
+    //estoy editando la peli
+  }
+}
+
+function creandoPelicula(){
+    console.log('aqui tengo que crear la peli')
 }
 
 //logica
-btnAgregarPelicula.addEventListener('click', mostrarModalPelicula)
+btnAgregarPelicula.addEventListener("click", mostrarModalPelicula);
+formularioPelicula.addEventListener("submit", administrarFormularioPelicula);
